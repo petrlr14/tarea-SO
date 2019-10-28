6e745ba26c5d111bb075f45fe90fa775448ef844
Pedro Gómez <pedrodeveloper14@gmail.com>
2019-10-28 14:54:44 -0600

    correccion en el ejercicio

--- 1.c
+++ 1.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
 void createSon();
 
-int main(int arg){
+/* Escribir N como parametro en la linea de comando */
+
+int main(int argc, char*argv[]){
+  int arg= atoi(argv[1]);
   printf("dad: %d\n", getpid());
   createSon(arg, arg);
   return 0;
@@ -15,8 +19,7 @@ void createSon(int count, int arg){
   if(count==0){
     return;
   }
-  int child=fork();
-  if(!child){
+  if(!fork()){
     for(int i=arg; i>=count; i--){
       printf("\t");
     }
