1c8ce4503047856e8a9920449d04d3cbe231c73c
Pedro Gómez <pedrodeveloper14@gmail.com>
2019-10-31 18:05:59 -0600

    terminado ejercicio 4

--- 4.c
+++ 4.c
@@ -1,128 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
 #include <signal.h>
-#include <sys/wait.h>
-
-/* Lista circular */
-struct node
-{
-    int val;
-    int index;
-    struct node *next;
-    struct node *prev;    
-};
-typedef struct node n;
-
-n* create_node(int, int);
-void add_node();
-void display_from_beg();
-void display_in_rev();
-/* ********************************* */
 
 /* Signal */
 void signalHandler(int signal);
-int reverse=0;
+int step=1, countNumber = 0;
 /* ********************************* */
 
 
-n *new, *ptr, *prev;
-n *first = NULL, *last = NULL;
-int number = 0, count=0;
-
 int main(int argc, char*argv[]){
-  int N = atoi(argv[1]);
-  int fd[2], process[N];
-  int hijo, father=getpid();
-  pipe(fd);
+  
+  int N = atoi(argv[1]), count=0, father=getpid(), canFork[N], process[N];
   for(int i=0; i<N; i++){
-    if(!fork()){
-      hijo = getpid();
-      count++;
-      close(fd[0]);
-      close(1);
-      dup(fd[1]);
-      write(1, &hijo, sizeof(hijo));
-      write(1, &count, sizeof(count));
-    }else{/* daddy */
-      close(0);
-      close(fd[1]);
-      dup(fd[0]);
-      read(fd[0], &hijo, sizeof(hijo));
-      read(fd[0], &count, sizeof(count));
-      add_node(hijo, count);
-      wait(NULL);
-    }
+    canFork[i]=1;
   }
-  if(father==getpid()){
-    signal(SIGINT, signalHandler);
-    ptr=first;
-    while(1){
-      display_from_beg();
-      display_in_rev();
-    }
-  }else{
-    kill(getpid(), SIGKILL);
-    wait(NULL);
+  signal(SIGTSTP, signalHandler);
+  signal(SIGPWR, signalHandler);
+  for(int i=0; i<N; i++){
+      if(getpid()==father){
+        if(canFork[i]){
+          canFork[i]=0;
+          int child=fork();
+          if(child>0){
+            process[i]=child;
+          }else{
+            countNumber=i+1;
+          }
+        }
+      }
   }
-}
-
-n* create_node(int info, int count){
-  new = (n *)malloc(sizeof(n));
-  new->val = info;
-  new->index=count;
-  new->next = NULL;
-  new->prev = NULL;
-  return new;
-}
-
-
-void add_node(int info, int count){
-  new = create_node(info, count);
-  if (first == last && first == NULL){
-    first = last = new;
-    first->next = last->next = NULL;
-    first->prev = last->prev = NULL;
-  }else{
-    last->next = new;
-    new->prev = last;
-    last = new;
-    last->next = first;
-    first->prev = last;
-  }
-}
-
-void display_from_beg(){
+  
   while(1){
-    if(reverse){
-      break;
-    }
-    printf("proceso hijo #%d: %d\n",ptr->index, ptr->val);
-    sleep(1);
-    ptr = ptr->next;
-  }
-}
-
-void display_in_rev(){
-  while(1){
-    if(!reverse){
-      break;
-    }
-    printf("proceso hijo #%d: %d\n",ptr->index, ptr->val);
-    sleep(1);
-    ptr=ptr->prev;
+   if(father==getpid()){   
+      for(long i=0; i<100000000; i++){}
+      kill(process[count], SIGPWR);
+      count+=step;
+      if(count==N){
+        count=0;
+      }
+      if(count<0){
+        count=N-1;
+      }
+    }else{
+      pause();
+      printf("proceso hijo #%d: %d\n", countNumber ,getpid());
+    } 
   }
 }
 
 void signalHandler(int signal){
   switch (signal){
-  case SIGINT:
-    if(reverse)
-      reverse=0;
-    else
-      reverse=1;
+  case SIGTSTP:
+    step*=-1;
+    for(int i=0; i<10000000; i++){}
     break;
+  case SIGPWR:
   default:
     break;
   }
