633442c43b66d1c168b7c64106ff2b483f85ec50
Andres <00099216@uca.edu.sv>
2019-11-03 00:21:19 -0600

    ahora si ya esta

--- 7.c
+++ 7.c
@@ -2,16 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define NUM_THREADS 2
-static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+#define NUM_THREADS 3
+pthread_mutex_t mutex [100] = PTHREAD_MUTEX_INITIALIZER;
 int cantidad;
 int Arreglo[100];
 int numero;
 
+pthread_t threads[NUM_THREADS]; 
+int error;
+int error1; 
+int error2;
+
 void *funcionThreadOrdenarDerecha (void *args);
 void *funcionThreadOrdenarIzquierda (void *args);
+void *funcionThreadPedir(void *args);
 
 void swap(int *xp, int *yp){ 
+    
     int temp = *xp; 
     *xp = *yp; 
     *yp = temp; 
@@ -21,37 +28,64 @@ void bubbleSortIzquierda(){
    int i, j; 
     for (i = 0; i < cantidad-1; i++) {
         for (j = 0; j < cantidad-i-1; j++) {
-            pthread_mutex_lock(&mutex);
-            printf("esta hilo 2\n");
+            pthread_mutex_lock(&mutex[j]);
+            pthread_mutex_lock(&mutex[j+1]);
             if (Arreglo[j] > Arreglo[j+1]){
                 swap(&Arreglo[j], &Arreglo[j+1]); 
             }
-            pthread_mutex_unlock(&mutex);
+            pthread_mutex_unlock(&mutex[j]);
+            pthread_mutex_unlock(&mutex[j+1]);
+            
        }
-
-   }      
-
+   }
 } 
 
 void bubbleSortDerecha(){ 
    int i, j; 
-   for (i = cantidad-1; i >=0; i--){
+   for (i = 0; i < cantidad; i++){
         for (j = cantidad-1-i; j >0; j--){
-            pthread_mutex_lock(&mutex); 
-            printf("esta hilo 1\n");
+            pthread_mutex_lock(&mutex[j]);
+            pthread_mutex_lock(&mutex[j-1]);
             if (Arreglo[j] < Arreglo[j-1]){
                 swap(&Arreglo[j-1], &Arreglo[j]);
             }
-            pthread_mutex_unlock(&mutex);     
+            pthread_mutex_unlock(&mutex[j]);
+            pthread_mutex_unlock(&mutex[j-1]);
+                
         }
     }
 }
 
 int main() {
-    pthread_t threads[NUM_THREADS]; 
-    int error1; 
-    int error2;
+	error = pthread_create (&threads[0], NULL, funcionThreadPedir, NULL);
+    if (error != 0)
+	{
+		perror ("No puedo crear thread principal");
+		exit (-1);
+	}
+    pthread_join(threads[0], NULL);
+    for(int i =0; i<cantidad; i++){
+        printf("%d ", Arreglo[i] );
+    }
+    printf("\n");
+    pthread_exit(NULL);
+}
 
+void *funcionThreadOrdenarDerecha (void *args){
+    
+    bubbleSortDerecha();
+    pthread_exit(NULL);
+
+}
+
+void *funcionThreadOrdenarIzquierda (void *args){
+     
+    bubbleSortIzquierda();
+    pthread_exit(NULL);
+}
+
+//FUNCION PEDIR
+void *funcionThreadPedir(void *args){
     printf("Introduzca la cantidad de numeros a ingresar(no mayor a 100): ");
     scanf("%d", &cantidad);
 
@@ -62,44 +96,20 @@ int main() {
     }
 
     /* Creamos los thread */
-	error1 = pthread_create (&threads[0], NULL, funcionThreadOrdenarDerecha, NULL);
+	error1 = pthread_create (&threads[1], NULL, funcionThreadOrdenarDerecha, NULL);
     if (error1 != 0)
 	{
 		perror ("No puedo crear thread 1");
 		exit (-1);
 	}
-    error2 = pthread_create (&threads[1], NULL, funcionThreadOrdenarIzquierda, NULL);
+    error2 = pthread_create (&threads[2], NULL, funcionThreadOrdenarIzquierda, NULL);
     if (error2 != 0)
 	{
 		perror ("No puedo crear thread 2");
 		exit (-1);
 	}
-    for(int i =0; i<NUM_THREADS; i++){
+    for(int i =1; i<NUM_THREADS; i++){
         pthread_join(threads[i], NULL);
-    }
-    pthread_exit(NULL);
-}
-
-void *funcionThreadOrdenarDerecha (void *args){
-
-    
-    printf("estoy en el hilo 1\n");
-    
-    bubbleSortDerecha();
-    for(int i =0; i<cantidad; i++){
-        printf("la posicion %d en el hilo 1 es: %d\n", i+1, Arreglo[i] );
-    }
-    pthread_exit(NULL);
-
-}
-
-void *funcionThreadOrdenarIzquierda (void *args){
-  
-    printf("estoy en el hilo 2\n");
-    
-    bubbleSortIzquierda();
-    for(int i =0; i<cantidad; i++){
-        printf("la posicion %d en el hilo 2 es: %d\n", i+1, Arreglo[i] );
     }
     pthread_exit(NULL);
 }
