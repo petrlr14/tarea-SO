611d0063fd7720015cd8d6a66b974229134f2732
nelsoncaastro <00043516@uca.edu.sv>
2019-11-03 21:36:43 -0600

    Solo faltan los argumentos

--- 3.c
+++ 3.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-int main(int argc, char*argv[]){
+int main(void){
+    char *buf = malloc(1);    
+    pid_t pid; 
+    puts("Bienvenido/a a la consola pirata v0.9");
+    printf("so@agave -> ");
+    char **argv = (char **)malloc(sizeof(char *));
+    while (1){
+        fgets(buf, sizeof(char *), stdin);
+        argv[0]=buf;
+        argv[1]=NULL;
+
+        pid = fork();
+        if(pid == 0){
+            
+        //if(buf == "exit") break;
+            
+            execve(argv[0],argv,NULL);
+        }else{
+            wait(NULL);
+            printf("so@agave -> ");
+        }
+        free(buf);
+    }
+    puts("Saliendo de la consola pirata v0.9"); 
     return 0;
 }
